81be980991948dc3f0f22d7c4d632cdffbefa8a1
Aliwoto <aminnimaj@gmail.com>
2022-12-04 02:49:47 +0330

    Add some more examples.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- src/Phase1/SQLWhere/sql_where01.sql
+++ src/Phase1/SQLWhere/sql_where01.sql
@@ -1,5 +1,5 @@
 -- The following SQL statement selects all the customers from the 
--- country "Mexico", in the "Customers" table:
+-- country "Mexico", in the "Customers" table: (See also: https://www.w3schools.com/sql/sql_where.asp)
 SELECT * FROM Customers
     WHERE Country = 'Mexico';
 -- or for numbers
@@ -37,6 +37,9 @@ SELECT * FROM Products
 -- `!=` : Not equal
 SELECT * FROM Products
     WHERE Price != 18;
+-- or
+SELECT * FROM Products
+    WHERE NOT Price = 18;
 
 -- `BETWEEN` : Between a certain range
 SELECT * FROM Products
@@ -49,3 +52,28 @@ SELECT * FROM Customers
 -- `IN` : 	To specify multiple possible values for a column
 SELECT * FROM Customers
     WHERE City IN ('Paris','London');
+
+
+SELECT * FROM Customers
+    WHERE City = 'Berlin' OR City = 'London';
+
+
+-- The following SQL statement selects all fields from 
+-- "Customers" where country is "Germany" AND city is "Berlin":
+SELECT * FROM Customers
+    WHERE Country = 'Germany' AND City = 'Berlin';
+
+-- The following SQL statement selects all fields from \
+-- Customers" where city is "Berlin" OR "München":
+SELECT * FROM Customers
+    WHERE City = 'Berlin' OR City = 'München';
+
+-- The following SQL statement selects all fields from
+-- "Customers" where country is "Germany" OR "Spain":
+SELECT * FROM Customers
+    WHERE Country = 'Germany' OR Country = 'Spain';
+
+-- The following SQL statement selects all fields from 
+-- "Customers" where country is NOT "Germany" and NOT "USA":
+SELECT * FROM Customers
+    WHERE NOT Country = 'Germany' AND NOT Country = 'USA';
